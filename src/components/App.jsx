242e6d19fa495df12ab727329527ee7c991f54df
Yaroslav Protsiuk <jaroslav.procjuk@gmail.com>
2023-05-09 23:57:41 +0300

    зробив компоненти та розмітку форми

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+// import { nanoid } from 'nanoid';
+//  import { idGenerator } from 'idgenerator-uuid-v4';
+
+// import Filter from './Filter/Filter';
+// import ContactList from './ContactList/ContactList';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+          flexDirection: 'column',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <ContactForm />
+
+        <h2>Contacts</h2>
+        {/* <Filter />
+        <ContactList /> */}
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import css from 'components/ContactForm/ContactForm.module.css';
+// import PropTypes from 'prop-types';
+const ContactForm = () => {
+  return (
+    <form className={css.form} name="contact_form" autoComplete="off">
+      <label className={css.label}>
+        Name
+        <input
+          className={css.input}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+
+      <label className={css.label}>
+        Number
+        <input
+          className={css.input}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
+
+      <button className={css.button} type="button">
+        Add contact
+      </button>
+    </form>
+  );
+};
+export default ContactForm;
